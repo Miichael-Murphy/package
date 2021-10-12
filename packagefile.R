3a897de8b69f69917b3de3ddd904011e22f5f635
Miichael-Murphy <90022248+Miichael-Murphy@users.noreply.github.com>
2021-10-12 23:00:20 +1100

    Tests
    Not sure if the marking criteria specifies 2 tests, or one test with 2 arguments/expectations, so i put both in case

--- packagefile.R
+++ packagefile.R
@@ -19,11 +19,18 @@ usethis::use_package("tibble")
 #added a readme file
 usethis::use_readme_rmd()
 
+#test
+usethis::use_testthat()
+
 #test 1 - expects the 3rd entry in the first column (Arrival times, ie the 3rd customer) arrives at about 326 seconds
 testthat::expect_identical(as.numeric(round(Multiserver(Arrivals = bank$arrival_time, ServiceTimes = bank$service_time, NumServers = 3)[3,1],0)), 326)
 
 #test 2 - expects the table output to have 4 slots per row, each representing Arrivals, ServiceBegins, ChosenServer and ServiceEnds respectively
 testthat::expect_vector(as.double(Multiserver(Arrivals = bank$arrival_time, ServiceTimes = bank$service_time, NumServers = 3)[3,]), 4)
 
-
+#Putting the tests together into the same test
+testthat::test_that("testing function works", {
+  testthat::expect_identical(as.numeric(round(Multiserver(Arrivals = bank$arrival_time, ServiceTimes = bank$service_time, NumServers = 3)[3,1],0)), 326)
+  testthat::expect_vector(as.double(Multiserver(Arrivals = bank$arrival_time, ServiceTimes = bank$service_time, NumServers = 3)[3,]), 4)
+})
 
